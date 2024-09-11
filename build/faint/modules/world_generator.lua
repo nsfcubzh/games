dc8dc1c650e460364b2f38e70e6e3f690258e53f
Nanskip <nanskip.oof@gmail.com>
2024-09-11 19:02:55 +0300

    added plants generation and returning world

--- build/faint/modules/world_generator.lua
+++ build/faint/modules/world_generator.lua
@@ -11,7 +11,7 @@ NSFLua['faint\\modules\\world_generator.lua'].LAST_SECTION_LINE = 0
 
 local worldgen = {}
 
-function worldgen.Generate(config)
+function worldgen.Generate(config) 
     if config == nil then
         error("worldgen.Generate(config) - 1st argument must be a table.")
     end
@@ -21,25 +21,34 @@ function worldgen.Generate(config)
         height = 200,
         zoom = 0.1,
         seed = 1,
-        octaves = 2,
-        waterLevel = 0.2,
-        sandLevel = 0.4,
-        grassLevel = 0.7,
+        octaves = 3,
+        contrast = 1.1,
+        erosion = 1.1,
+        waterLevel = 0.3,
+        sandLevel = 0.35,
+        grassLevel = 0.55,
+        podzoleLevel = 0.75,
         gravelLevel = 0.8,
         graniteLevel = 1,
 
         plants = {
             tree = {
-                scale = 0.2,
-                seed = 1,
+                zoom = 0.02,
+                seed = 2,
                 octaves = 1,
-                chance = 0.5
+                chances = {
+                    grass = 0.05,
+                    podzole = 0.6
+                }
             },
             grass = {
-                scale = 0.3,
-                seed = 2,
-                octaves = 2,
-                chance = 0.6
+                zoom = 0.03,
+                seed = 3,
+                octaves = 1,
+                chances = {
+                    grass = 0.5,
+                    podzole = 0.2
+                }
             },
         }
     }
@@ -59,9 +68,25 @@ function worldgen.Generate(config)
             world[x][y] = {}
             local cell = world[x][y]
 
-            local height = (perlin.get(x*cfg.zoom, y*cfg.zoom)+1)/2 -- getting height value from 0 to 1
+            local height = 0
+            local amplitude = 1
+            local frequency = 1
+            local max_amp = 0
+            
+            for i = 1, cfg.octaves do
+                local result = perlin.get(x * cfg.zoom * frequency, y * cfg.zoom * frequency)
 
-            cell.block = "debug"
+                height = height + result * amplitude
+                
+                max_amp = max_amp + amplitude
+                amplitude = amplitude / 2
+                frequency = frequency * 2
+            end
+
+            height = (((height + 1) / 2)^cfg.erosion) * cfg.contrast
+
+            height = math.min(math.max(height, 0), 1)
+
             if height < 0 then
                 cell.block = "debug"
             elseif height < cfg.waterLevel then
@@ -70,33 +95,55 @@ function worldgen.Generate(config)
                 cell.block = "sand"
             elseif height < cfg.grassLevel then
                 cell.block = "grass"
+            elseif height < cfg.podzoleLevel then
+                cell.block = "podzole"
             elseif height < cfg.gravelLevel then
                 cell.block = "gravel"
             elseif height <= cfg.graniteLevel then
                 cell.block = "granite"
+            else
+                cell.block = "error"
+            end
+        end
+    end
+    for name, plant in pairs(cfg.plants) do
+        perlin.seed(plant.seed)
+
+        for x = 1, cfg.width do
+            for y = 1, cfg.height do
+                local cell = world[x][y]
+
+                local chance = 0
+                local amplitude = 1
+                local frequency = 1
+                local max_amp = 0
+                
+                for i = 1, plant.octaves do
+                    local result = perlin.get(x * plant.zoom * frequency, y * plant.zoom * frequency)
+    
+                    chance = chance + result * amplitude
+                    
+                    max_amp = max_amp + amplitude
+                    amplitude = amplitude / 2
+                    frequency = frequency * 2
+                end
+    
+                chance = ((chance + 1) / 2)^4
+
+                if plant.chances[cell.block] ~= nil then
+                    chance = chance * plant.chances[cell.block]
+                end
+    
+                local result = math.random(0, 1/chance)
+
+                if result == 0 and cell.object == nil and plant.chances[cell.block] ~= nil then
+                    cell.object = name
+                end
             end
         end
     end
 
-    for x = 1, cfg.width do
-        local text = ""
-        for y=1, cfg.height do
-            if world[x][y].block == "debug" then
-                text = text .. " "
-            elseif world[x][y].block == "water" then
-                text = text .. "."
-            elseif world[x][y].block == "sand" then
-                text = text .. ":"
-            elseif world[x][y].block == "grass" then
-                text = text .. "|"
-            elseif world[x][y].block == "gravel" then
-                text = text .. "I"
-            elseif world[x][y].block == "granite" then
-                text = text .. "#"
-            end
-        end
-        print(text)
-    end
+    return world
 end
 
 return worldgen
