efd36ed5b2c3d4f676990d05a093dd2afe04e413
Nanskip <nanskip.oof@gmail.com>
2024-09-12 14:11:57 +0300

    added structure generation

--- build/faint/modules/world_generator.lua
+++ build/faint/modules/world_generator.lua
@@ -11,19 +11,21 @@ NSFLua['faint\\modules\\world_generator.lua'].LAST_SECTION_LINE = 0
 
 local worldgen = {}
 
-function worldgen.Generate(config) 
+function worldgen.Generate(config)
+    --NSFLua['faint\\modules\\world_generator.lua'].LAST_SECTION = "WORLD GENERATION" NSFLua['faint\\modules\\world_generator.lua'].LAST_SECTION_LINE = 4 Debug.log("faint\\modules\\world_generator.lua > New section: '".."WORLD GENERATION".."' [Line: 4]")
+
     if config == nil then
         error("worldgen.Generate(config) - 1st argument must be a table.")
     end
 
     local defaultConfig = {
-        width = 64,
-        height = 200,
-        zoom = 0.1,
+        width = 512,
+        height = 512,
+        zoom = 0.015,
         seed = 1,
-        octaves = 3,
-        contrast = 1.1,
-        erosion = 1.1,
+        octaves = 5,
+        contrast = 1.3,
+        erosion = 1.2,
         waterLevel = 0.3,
         sandLevel = 0.35,
         grassLevel = 0.55,
@@ -31,7 +33,7 @@ function worldgen.Generate(config)
         gravelLevel = 0.8,
         graniteLevel = 1,
 
-        plants = {
+        items = {
             tree = {
                 zoom = 0.02,
                 seed = 2,
@@ -50,6 +52,48 @@ function worldgen.Generate(config)
                     podzole = 0.2
                 }
             },
+            stone = {
+                zoom = 0.025,
+                seed = 4,
+                octaves = 2,
+                chances = {
+                    grass = 0.1,
+                    podzole = 0.2,
+                    gravel = 0.3,
+                }
+            },
+        },
+        structures = {
+            abandoned_room = {
+                chance = 0.001,
+                min_scale = {5, 5},
+                max_scale = {9, 9},
+                floor = true,
+                removed_walls = 0.8,
+                removed_floors = 0.8,
+                wall_type = "wood",
+                floor_type = "wood",
+                items = {
+                    blue = {
+                        chance = 0.01,
+                        scale = {1, 1}
+                    },
+                    red = {
+                        chance = 0.02,
+                        scale = {1, 1}
+                    },
+                    black = {
+                        chance = 0.01,
+                        scale = {2, 2}
+                    }
+                },
+                allowed_materials = {
+                    sand = true,
+                    grass = true,
+                    podzole = true,
+                    gravel = true,
+                }
+            }
         }
     }
     local cfg = {}
@@ -60,8 +104,12 @@ function worldgen.Generate(config)
         cfg[key] = value
     end
 
+    --Debug.log("world_generator - config saved in ["..cfg.."].")
+
     local world = {}
     perlin.seed(cfg.seed)
+
+    --Debug.log("world_generator - generating landscape...")
     for x = 1, cfg.width do
         world[x] = {}
         for y = 1, cfg.height do
@@ -106,8 +154,9 @@ function worldgen.Generate(config)
             end
         end
     end
-    for name, plant in pairs(cfg.plants) do
-        perlin.seed(plant.seed)
+    --Debug.log("world_generator - placing objects...")
+    for name, item in pairs(cfg.items) do
+        perlin.seed(item.seed)
 
         for x = 1, cfg.width do
             for y = 1, cfg.height do
@@ -118,8 +167,8 @@ function worldgen.Generate(config)
                 local frequency = 1
                 local max_amp = 0
                 
-                for i = 1, plant.octaves do
-                    local result = perlin.get(x * plant.zoom * frequency, y * plant.zoom * frequency)
+                for i = 1, item.octaves do
+                    local result = perlin.get(x * item.zoom * frequency, y * item.zoom * frequency)
     
                     chance = chance + result * amplitude
                     
@@ -130,20 +179,168 @@ function worldgen.Generate(config)
     
                 chance = ((chance + 1) / 2)^4
 
-                if plant.chances[cell.block] ~= nil then
-                    chance = chance * plant.chances[cell.block]
+                if item.chances[cell.block] ~= nil then
+                    chance = chance * item.chances[cell.block]
                 end
     
                 local result = math.random(0, 1/chance)
 
-                if result == 0 and cell.object == nil and plant.chances[cell.block] ~= nil then
+                if result == 0 and cell.object == nil and item.chances[cell.block] ~= nil then
                     cell.object = name
                 end
             end
         end
     end
 
+    --Debug.log("world_generator - placing structures...")
+    for name, structure in pairs(cfg.structures) do
+        for x = 1, cfg.width do
+            for y = 1, cfg.height do
+                --local cell = world[x][y]
+                if math.random(0, 1/structure.chance) == 0 then
+                    local directions = {
+                        x = {1, -1},
+                        y = {1, -1},
+                    }
+
+                    local direction = {directions["x"][math.random(1, 2)], directions["y"][math.random(1, 2)]}
+                    local scale = {math.random(structure.min_scale[1], structure.max_scale[1]), math.random(structure.min_scale[2], structure.max_scale[2])}
+
+                    for i = 1, scale[1] do 
+                        for j = 1, scale[2] do
+                            local cell = nil
+                            local cordX = x + (i * direction[1])
+                            local cordY = y + (j * direction[2])
+
+                            if world[cordX] ~= nil then
+                                if world[cordX][cordY] ~= nil then
+                                    cell = world[cordX][cordY]
+                                end
+                            end
+
+                            local block = nil
+                            if math.random(0, 1/(structure.removed_floors)) == 0 then
+                                block = "floor"
+                            end
+                            
+                            if cell ~= nil then
+                                if structure.allowed_materials[cell.block] and block ~= nil then
+                                    cell.block = block
+                                end
+
+                                for name, item in pairs(structure.items) do
+                                    if math.random(0, 1/(structure.items[name].chance)) == 0 then
+                                        if cell.object == nil and cell.block == "floor" then
+                                            cell.object = name
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+
+
+                    for i = 0, scale[1] do
+                        local cell = nil
+                        local cordX = x + (i * direction[1])
+
+                        if world[cordX] ~= nil then
+                            if world[cordX][y] ~= nil then
+                                cell = world[cordX][y]
+                            end
+                        end
+
+                        local object = nil
+                        if math.random(0, 1/(structure.removed_walls)) == 0 then
+                            object = "wall"
+                        end
+
+                        if cell ~= nil then
+                            if structure.allowed_materials[cell.block] and object ~= nil then
+                                cell.object = object
+                            end
+                        end
+                    end
+
+                    for j = 1, scale[2] do
+                        local cell = nil
+                        local cordY = y + (j * direction[2])
+
+                        if world[x] ~= nil then
+                            if world[x][cordY] ~= nil then
+                                cell = world[x][cordY]
+                            end
+                        end
+
+                        local object = nil
+                        if math.random(0, 1/(structure.removed_walls)) == 0 then
+                            object = "wall"
+                        end
+
+                        if cell ~= nil then
+                            if structure.allowed_materials[cell.block] and object ~= nil then
+                                cell.object = object
+                            end
+                        end
+                    end
+
+                    for i = 1, scale[1] do
+                        local cell = nil
+                        local cordX = x + (i * direction[1])
+
+                        if world[cordX] ~= nil then
+                            if world[cordX][y] ~= nil then
+                                cell = world[cordX][y+(direction[2]*scale[2])]
+                            end
+                        end
+
+                        local object = nil
+                        if math.random(0, 1/(structure.removed_walls)) == 0 then
+                            object = "wall"
+                        end
+
+                        if cell ~= nil then
+                            if structure.allowed_materials[cell.block] and object ~= nil then
+                                cell.object = object
+                            end
+                        end
+                    end
+
+                    for j = 1, scale[2] do
+                        local cell = nil
+                        local cordY = y + (j * direction[2])
+
+                        if world[x+(direction[1]*scale[1])] ~= nil then
+                            if world[x][cordY] ~= nil then
+                                cell = world[x+(direction[1]*scale[1])][cordY]
+                            end
+                        end
+
+                        local object = nil
+                        if math.random(0, 1/(structure.removed_walls)) == 0 then
+                            object = "wall"
+                        end
+
+                        if cell ~= nil then
+                            if structure.allowed_materials[cell.block] and object ~= nil then
+                                cell.object = object
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    --Debug.log("world_generator - World generation completed.")
+
     return world
+end
+
+function worldgen.Build(world)
+    if world == nil then
+        error("worldgen.Build(world) - 1st argument should be a world data.", 2)
+    end
 end
 
 return worldgen
