16efb39c68594472a061514e7f69acb0793ee107
Nanskip <nanskip.oof@gmail.com>
2024-09-19 16:56:03 +0300

    fixed collisionbox position

--- src/faint/game/game.lua
+++ src/faint/game/game.lua
@@ -141,9 +141,12 @@ function game.loadChunk(map, posX, posY)
                     game.data[originalX + 1][originalY + 1].shape.Scale = 0.07
                     game.data[originalX + 1][originalY + 1].shape.Position = Number3(originalX + 0.5, 1, originalY + 0.5) * map.Scale.X
                     game.data[originalX + 1][originalY + 1].shape.Physics = PhysicsMode.Disabled
+
                     game.data[originalX + 1][originalY + 1].collider = Object()
                     game.data[originalX + 1][originalY + 1].collider.CollisionBox = fixedBox(game.data[originalX + 1][originalY + 1].shape.CollisionBox)
-                    game.data[originalX + 1][originalY + 1].collider.Position = Number3(originalX, 1, originalY) * map.Scale.X
+                    local offsetx = (10 - game.data[originalX + 1][originalY + 1].collider.CollisionBox.Max.X)/2
+                    local offsetz = (10 - game.data[originalX + 1][originalY + 1].collider.CollisionBox.Max.Z)/2
+                    game.data[originalX + 1][originalY + 1].collider.Position = Number3(originalX+offsetx, 1, originalY-offsetz) * map.Scale.X
                     game.data[originalX + 1][originalY + 1].collider:SetParent(World)
 
                     if game.data[originalX + 1][originalY + 1].setScale ~= nil then
