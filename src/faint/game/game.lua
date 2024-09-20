dc6f780b238a5daae634f1dfa4e202a1c0765690
Nanskip <nanskip.oof@gmail.com>
2024-09-20 22:29:59 +0300

    trying to fix colliders, yeah

--- src/faint/game/game.lua
+++ src/faint/game/game.lua
@@ -157,17 +157,16 @@ function game.loadChunk(map, posX, posY)
                     game.data[originalX + 1][originalY + 1].shape.Position = Number3(originalX + 0.5, 1, originalY + 0.5) * map.Scale.X
                     game.data[originalX + 1][originalY + 1].shape.Physics = PhysicsMode.Disabled
 
+                    if game.data[originalX + 1][originalY + 1].update ~= nil then
+                        game.data[originalX + 1][originalY + 1]:update()
+                    end
+
                     game.data[originalX + 1][originalY + 1].collider = Object()
                     game.data[originalX + 1][originalY + 1].collider.CollisionBox = fixedBox(game.data[originalX + 1][originalY + 1].shape.CollisionBox)
                     local offsetx = (10 - game.data[originalX + 1][originalY + 1].collider.CollisionBox.Max.X)/2
                     local offsetz = (10 - game.data[originalX + 1][originalY + 1].collider.CollisionBox.Max.Z)/2
-                    game.data[originalX + 1][originalY + 1].collider:SetParent(game.data[originalX + 1][originalY + 1].shape)
                     game.data[originalX + 1][originalY + 1].collider.Position = Number3(originalX, 1, originalY) * map.Scale.X + Number3(offsetx, 0, offsetz)
-                    game.data[originalX + 1][originalY + 1].collider.Rotation = Rotation(0, 0, 0)
-
-                    if game.data[originalX + 1][originalY + 1].update ~= nil then
-                        game.data[originalX + 1][originalY + 1]:update()
-                    end
+                    game.data[originalX + 1][originalY + 1].collider:SetParent(World)
                 end
             end
         end
