eddbb4a41e2e7d639bcd4d0b31c1a6b74bec6a52
Nanskip <nanskip.oof@gmail.com>
2024-09-21 11:53:17 +0300

    fixed pe coords

--- build/faint/game/game.lua
+++ build/faint/game/game.lua
@@ -245,8 +245,7 @@ function game.initInventory()
     end, {topPriority = true})
     game.inventory.down = LocalEvent:Listen(LocalEvent.Name.PointerDown, function(pe)
         -- calls when pointer is down, clicking or touching screen
-
-        print("down")
+        local pe = Number2(pe.X*Screen.Width, pe.Y*Screen.Height)
         for i=0, #game.inventory.buttons-1 do
             local buttonpos = game.inventory.buttons[i].pos
             local buttonscale = Number2(game.inventory.buttons[i].Width, game.inventory.buttons[i].Height)
