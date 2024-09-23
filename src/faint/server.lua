78e2d192713ade0e299f133c9fabb141970df3d9
Nanskip <nanskip.oof@gmail.com>
2024-09-23 16:52:20 +0300

    decreased it back. yeah

--- src/faint/server.lua
+++ src/faint/server.lua
@@ -120,7 +120,7 @@ function doneLoading()
 		local e = Network.Event("start", {})
 		e:SendTo(p)
 	end
-	world_scale = 256
+	world_scale = 128
 	world_map = worldgen.Generate({width = world_scale, height = world_scale})
 end
 
